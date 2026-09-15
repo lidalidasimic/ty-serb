9730cea6b3de6eb72d0b24d49a09019b821e1061
lidalidasimic <lidalidasimic@gmail.com>
2026-09-16 00:15:55 +0300

    Render lesson two practice in course

--- app/lessons/[slug]/page.tsx
+++ app/lessons/[slug]/page.tsx
@@ -13,6 +13,7 @@ import { getLessonBySlug, lessons } from "@/data/lessons";
 import { canOpenLesson } from "@/lib/access-control";
 import { getCurrentUser, logActivity } from "@/lib/supabase-server";
 import LessonOneGammaExperience from "@/components/LessonOneGammaExperience";
+import LessonTwoPractice from "@/components/LessonTwoPractice";
 
 type LessonPageProps = {
   params: Promise<{
@@ -239,6 +240,7 @@ export default async function LessonPage({ params }: LessonPageProps) {
           <LessonOneGammaExperience isAuthenticated={Boolean(user)} />
         </>
       ) : null}
+      {lesson.number === 2 ? <LessonTwoPractice /> : null}
     </>
   );
 }
